3c1483b4b6e77d455dc3bda4edcb5f210c27d390
Ajay K Yadav <ajayadav.aj@gmail.com>
2026-07-30 23:50:10 +0530

    Add EF Core DbContext and User configuration

---
+++ backend/CareerOS.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,30 @@
+using CareerOS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CareerOS.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.FirstName)
+               .HasMaxLength(100)
+               .IsRequired();
+
+        builder.Property(x => x.LastName)
+               .HasMaxLength(100)
+               .IsRequired();
+
+        builder.Property(x => x.Email)
+               .HasMaxLength(255)
+               .IsRequired();
+
+        builder.HasIndex(x => x.Email)
+               .IsUnique();
+    }
+}
